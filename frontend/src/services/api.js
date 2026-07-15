b06896dc63c697eac054a267aeb040d4edee8dac
VirendrasinhJadeja <virendrasinhjadeja3026@gmail.com>
2026-07-16 02:47:14 +0530

    Debug API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+console.log("Axios Base URL =", API.defaults.baseURL);
 
 // Automatically attach JWT token
 API.interceptors.request.use((config) => {
